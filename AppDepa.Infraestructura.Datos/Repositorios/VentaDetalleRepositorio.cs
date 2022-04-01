06163c32013359c626452cc0eeb59a7a4f7be461
Mbarreto <mabt2206@gmail.com>
2022-04-01 01:55:34 -0500

    Nuevo ambiente de infraestructura para el proyecto PIT - Departamentos - mb.01.04.2022

---
+++ AppDepa.Infraestructura.Datos/Repositorios/VentaDetalleRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AppDepa.Dominio;
+using AppDepa.Dominio.Interfaces.Repositorios;
+using AppDepa.Infraestructura.Datos.Contextos;
+
+namespace AppDepa.Infraestructura.Datos.Repositorios
+{
+    public class VentaDetalleRepositorio : IRepositorioDetalle<VentaDetalle, Guid>
+    {
+        private VentaContexto db;
+
+        public VentaDetalleRepositorio(VentaContexto _db)
+        {
+            db = _db;
+        }
+
+        public VentaDetalle Agregar(VentaDetalle entidad)
+        {
+            db.VentaDetalles.Add(entidad);
+            return entidad;
+        }
+
+        public void GuardarTodosLosCambios()
+        {
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Departamento.Dominio/Interfaces/Repositorios/IRepositorioDetalle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDepa.Dominio.Interfaces.Repositorios
+{
+    public interface IRepositorioDetalle<TEntidad, TMovimientoID> : IAgregar<TEntidad>, ITransaccion
+    {
+
+    }
+}
