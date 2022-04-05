c84bad7a90dbc4366e1289c3616f34d82a12e095
MiguelProgrammerxcode <mabt2206@gmail.com>
2022-04-05 00:31:15 -0500

    confirm

---
+++ AppDepa.Dominio/DepartamentoDetalle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDepa.Dominio
+{
+    public class DepartamentoDetalle
+    {
+        public int PropietarioId { get; set; }
+        public int DepartamentoId { get; set; }
+        public int CantidadHabitaciones { get; set; }
+        public string IndCocina { get; set; }
+        public string IndBano { get; set; }
+        public string IndBalcon { get; set; }
+        public string IndLavanderia { get; set; }
+        public string IndPiscina { get; set; }
+        public string IndPatio { get; set; }
+        [Column(TypeName = "varchar(40)")]
+        public string ParametroId { get; set; }
+        // declarar public virtual Parametro Parametro { get; set; }
+        public string FechaRegistro { get; set; }
+    }
+}
