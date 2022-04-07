897b6c9b307ae6a46c76e070d8a5f32f6cc5c118
Light780 <brunorlm88@gmail.com>
2022-04-06 21:32:23 -0500

    PersonaMascota

--- AppDepa.Dominio/Departamento.cs
+++ AppDepa.Dominio/Departamento.cs
@@ -12,7 +12,8 @@ namespace AppDepa.Dominio
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]        
         public int DepartamentoId { get; set; }
         [Column(TypeName = "varchar(15)")]
-        public string NroDepartamento { get; set; }        
+        public string NroDepartamento { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Tamano { get; set; }        
         public int TipoDepaId { get; set; }
         public int EstadoDepaId { get; set; }
@@ -32,5 +33,7 @@ namespace AppDepa.Dominio
         public DateTime FechaRegistro { get; set; }
         public ICollection<Boleta> Boletas { get; set; }
         public ICollection<Incidencia> Incidencias { get; set; }
+        public ICollection<Mascota> Mascotas { get; set; }
+        public ICollection<Persona> Personas { get; set; }
     }
 }
