5b3f7b554c6690106d052bf2573456619c8f4d77
Light780 <brunorlm88@gmail.com>
2022-06-11 21:35:13 -0500

    Boleta y Pago Servicio DTO's

---
+++ AppDepa.Aplicaciones/Dto/PagoServicioDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDepa.Aplicaciones.Dto
+{
+    public class PagoServicioDto
+    {
+        public int PagoServicioId { get; set; }
+        public string CodigoPago { get; set; } //Viene de tabla Boleta
+        public string NroOperacion { get; set; }
+        public string FechaPago { get; set; }
+        public string Usuario { get; set; }
+        public decimal Monto { get; set; }
+        public string Persona { get; set; } //Nombre de la persona que cancelo, viene de tabla Persona
+    }
+}
