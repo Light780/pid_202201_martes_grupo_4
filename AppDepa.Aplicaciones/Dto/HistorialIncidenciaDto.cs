950ae04a17c17f62d1dc82a0231748dc79ec8a83
Araceli Gutarra Soto <aragutarras@gmail.com>
2022-06-14 23:22:46 -0500

    Funcionalidad backend para listar Historial Incidencias culminado

---
+++ AppDepa.Aplicaciones/Dto/HistorialIncidenciaDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDepa.Aplicaciones.Dto
+{
+    public class HistorialIncidenciaDto
+    {
+        //Datos de la Incidencia
+        public int HistorialIncidenciaId { get; set; }
+        public string CodigoIncidencia { get; set; }
+        public string NroDepartamento { get; set; }
+        public string TipoDepartamento { get; set; }
+        public string TipoIncidencia { get; set; }
+        public string DescripcionIncidencia { get; set; }
+        public string EstadoIncidencia { get; set; }
+        public string FechaIncidencia { get; set; }
+        //Usuario
+        public string UsuarioRegistro { get; set; }
+        public string FechaRegistro { get; set; }
+        //Informante
+        public string Informante { get; set; }
+    }
+}
