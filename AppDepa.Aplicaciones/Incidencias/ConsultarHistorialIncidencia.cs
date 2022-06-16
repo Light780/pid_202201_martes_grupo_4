8c051899b015ea104f99e23f79ae981d03bd24f6
MiguelBarretoBD <102126994+MiguelProgrammerxcode@users.noreply.github.com>
2022-06-16 00:07:39 -0500

    confirm-16.06.2022

--- AppDepa.Aplicaciones/Incidencias/ConsultarHistorialIncidencia.cs
+++ AppDepa.Aplicaciones/Incidencias/ConsultarHistorialIncidencia.cs
@@ -31,32 +31,33 @@ namespace AppDepa.Aplicaciones.Incidencias
             }
             public async Task<List<HistorialIncidenciaDto>> Handle(ListarHistorial request, CancellationToken cancellationToken)
             {
- 
-                var query = from h in context.HistorialIncidencia
-                            join p in context.Persona on h.PersonaId equals p.PersonaId
-                            join d in context.Departamento on h.DepartamentoId equals d.DepartamentoId
-                            join i in context.Incidencia on h.IncidenciaId equals i.IncidenciaId
-                            join u in context.Usuario on h.UsuarioId equals u.UsuarioId
-                            orderby h.HistorialIncidenciaId
-                            where (request.DepartamentoId == 0 || h.Departamento.DepartamentoId == request.DepartamentoId)
-                            where (request.TipoIncidenciaId == 0 || h.TipoIncidenciaId == request.TipoIncidenciaId)
-                            select new HistorialIncidenciaDto
-                            {
-                                HistorialIncidenciaId = h.HistorialIncidenciaId,
-                                CodigoIncidencia = i.CodigoIncidencia,
-                                NroDepartamento = d.NroDepartamento,
-                                TipoDepartamento = utils.BuscarParametro(d.TipoDepaId, "TIPO_DEPA_ID"),
-                                TipoIncidencia = utils.BuscarParametro(h.TipoIncidenciaId, "TIPO_INCIDENCIA_ID"),
-                                DescripcionIncidencia = h.DescripcionIncidencia,
-                                EstadoIncidencia = utils.BuscarParametro(h.EstadoIncidenciaId, "ESTADO_INCIDENCIA_ID"),
-                                FechaIncidencia = h.FechaIncidencia.ToString("dd/MM/yyyy HH:mm"),
-                                UsuarioRegistro = u.UserName,
-                                FechaRegistro = h.FechaRegistro.ToString("dd/MM/yyyy HH:mm"),
-                                Informante = p.NombreCompleto
-                            };
+                return null;
+                //var query = from h in context.HistorialIncidencia
+                //            join p in context.Persona on h.PersonaId equals p.PersonaId
+                //            join d in context.Departamento on h.DepartamentoId equals d.DepartamentoId
+                //            join i in context.Incidencia on h.IncidenciaId equals i.IncidenciaId
+                //            join u in context.Usuario on h.UsuarioId equals u.UsuarioId
+                //            orderby h.HistorialIncidenciaId
+                //            where (request.DepartamentoId == 0 || h.Departamento.DepartamentoId == request.DepartamentoId)
+                //            where (request.TipoIncidenciaId == 0 || h.TipoIncidenciaId == request.TipoIncidenciaId)
+                //            select new HistorialIncidenciaDto
+                //            {
+                //                HistorialIncidenciaId = h.HistorialIncidenciaId,
+                //                CodigoIncidencia = i.CodigoIncidencia,
+                //                NroDepartamento = d.NroDepartamento,
+                //                TipoDepartamento = utils.BuscarParametro(d.TipoDepaId, "TIPO_DEPA_ID"),
+                //                TipoIncidencia = utils.BuscarParametro(h.TipoIncidenciaId, "TIPO_INCIDENCIA_ID"),
+                //                DescripcionIncidencia = h.DescripcionIncidencia,
+                //                EstadoIncidencia = utils.BuscarParametro(h.EstadoIncidenciaId, "ESTADO_INCIDENCIA_ID"),
+                //                FechaIncidencia = h.FechaIncidencia.ToString("dd/MM/yyyy HH:mm"),
+                //                UsuarioRegistro = u.UserName,
+                //                FechaRegistro = h.FechaRegistro.ToString("dd/MM/yyyy HH:mm"),
+                //                Informante = p.NombreCompleto
+                //            };
 
-                return await query.ToListAsync();
+                //return await query.ToListAsync();
                 //
+                
             }
         }
     }
